0b7a427f7b5b3e8de6e2734965ac682a0d896f31
JairoDH <123737760+JairoDH@users.noreply.github.com>
2023-03-07 01:00:04 +0100

    modificacion
    Modificación de nuevos errores

--- Funciones.py
+++ Funciones.py
@@ -24,6 +24,7 @@ def menu():
     opcion = int(input("Selecciona una opcion: "))
     return opcion
 
+
 #consultanumero1
 def mostrar_trabajadores(db):
        
@@ -34,34 +35,32 @@ def mostrar_trabajadores(db):
         registros = cursor.fetchall()
         for registro in registros:
             print(registro[0],registro[1],registro[2],registro[3])
-    except cx_Oracle.DatabaseError as e:
-        error, = e.args
-        print("Error en la consulta: ", error.message)
-    finally:
-        db.close()
+        return registros
+    except:
+        print("Error en la consulta.")
+  
 
 
 #consultanumero2
 
-def informacion_telefono(db):
+def informacion_codigo(db):
     
     codigo = int(input("Introduzca el código del trabajador: "))
 
-    sql = "SELECT * FROM CONDUCTOR WHERE codigo = :codigo"
+    sql = "SELECT * FROM CONDUCTOR WHERE codigo = %s"
     cursor = db.cursor()
 
     try:     
-        cursor.execute(sql,{'codigo':codigo})
+        cursor.execute(sql, (codigo,))
         registro = cursor.fetchone()
         while registro:
             print(registro[0],registro[1],registro[2],registro[3],registro[4],registro[5],registro[6],registro[7],registro[8],registro[9])
             registro = cursor.fetchone()
-    except cx_Oracle.DatabaseError as e:
-        error, = e.args
-        print("Error en la consulta: ", error.message)
-    finally:
-        db.close()
-    
+
+    except:
+        print("Error en la consulta")
+    db.close()
+
 
 #consultanumero3
 
@@ -69,41 +68,36 @@ def informacion_matricula(db):
 
     matricula_camion = input("Introduzca la matrícula del camión: ")
 
-    sql = "SELECT * FROM CAMION_CONDUCTOR cc JOIN CONDUCTOR c ON cc.codigo_conductor = c.codigo JOIN CAMION ca ON cc.matricula_camion = ca.matricula WHERE ca.matricula = :matricula_camion"
+    sql = "SELECT * FROM CAMION_CONDUCTOR cc JOIN CONDUCTOR c ON cc.codigo_conductor = c.codigo JOIN CAMION ca ON cc.matricula_camion = ca.matricula WHERE ca.matricula = %s"
     cursor = db.cursor()
 
     try:
-        cursor.execute(sql, {'matricula_camion':matricula_camion})
-        registro = cursor.fetchone()
-        while registro:
+        cursor.execute(sql, (matricula_camion,))
+        registros = cursor.fetchall()
+        for registro in registros:
             print(registro[0],registro[1],registro[2],registro[3],registro[4],registro[5],registro[6],registro[7],registro[8],registro[9])
-            registro = cursor.fetchone()
-    except cx_Oracle.DatabaseError as e:
-        error, = e.args
-        print("Error en la consulta: ", error.message)
-    finally:
-        db.close()
+        return registros
+    except:
+        print("Error en la consulta")
+
 
 #consultanumero4
 
 def nuevo_camion(db):
 
-    matri = input("Introduce la matrícula (AAAA000): ")
+    matri = input("Introduce la matrícula (0000AAA): ")
     fecha = input("Introduce la fecha de alta (YYYY-MM-DD): ")
     peso = int(input("Introduce el peso máximo a transportar: "))
 
-    sql = "INSERT INTO CAMION (matricula, fecha_alta, peso_maximo) VALUES (:matri, :fecha, :peso)"
+    sql = "INSERT INTO CAMION (matricula, fecha_alta, peso_maximo) VALUES ('{matri}', '{fecha}', '{peso}')"
     cursor = db.cursor()
 
     try:
-        cursor.execute(sql, {'matri': matri ,'fecha': fecha,'peso': peso})
+        cursor.execute(sql.format(matri=matri, fecha=fecha, peso=peso))
         db.commit()
-    except cx_Oracle.DatabaseError as e:
-        error, = e.args
-        print("Error en la consulta: ", error.message)
-    finally:
-        db.close()
-       
+    except:
+        db.rollback()
+    db.close()
 
 #consultanumero5
 
@@ -112,27 +106,24 @@ def eliminar_camion(db):
     dni = input("Introduce el DNI: ")
 
 
-    sql = "SELECT matricula_camion FROM CAMION_CONDUCTOR WHERE codigo_conductor = ( SELECT codigo FROM CONDUCTOR where DNI = :dni)"
+    sql = "SELECT matricula_camion FROM CAMION_CONDUCTOR WHERE codigo_conductor = ( SELECT codigo FROM CONDUCTOR where DNI = %s)"
     cursor = db.cursor()
 
     try:
-        cursor.execute(sql, {'dni': dni})
+        cursor.execute(sql, (dni))
         resultado = cursor.fetchone()
         if resultado is not None:
             matricula_camion = resultado[0]
-            borrar = "DELETE FROM CAMION WHERE matricula = :matricula_camion"
-            cursor.execute(borrar, {'matricula_camion': matricula_camion})
+            borrar = "DELETE FROM CAMION_CONDUCTOR WHERE matricula_camion = %s"
+            borrar2 = "DELETE FROM CAMION WHERE matricula  = %s"
+            cursor.execute(borrar, (matricula_camion))
+            cursor.execute(borrar2, (matricula_camion))
             db.commit()
-            print("Camión con matrícula {matricula_camion} se ha eliminado correctamente.")
-        else:
-            print("No hay camión asignado al DNI {dni}.")
-   
-    except cx_Oracle.DatabaseError as e:
-        error, = e.args
-        print('Error en la consulta: ', error.message)
-        db.rollback()
-    finally:
-        db.close()
+            print("Camión eliminado correctamente.")
+    except MySQLdb.Error as e:
+        print(e)
+        sys.exit(1)
+    
 
 #consultanumero6
 
@@ -145,19 +136,17 @@ def actualizar_trabajador(db):
     nuevo_municipio = input("Introduce el nuevo municipio: ")
 
 
-    sql = "UPDATE CONDUCTOR SET telefono = :nuevo_telefono, poblacion = nuevo_municipio WHERE nombre = :nombre AND apellido1 = :apellido1"
+    sql = "UPDATE CONDUCTOR SET telefono = %s, poblacion = %s WHERE nombre = %s AND apellido1 = %s"
     cursor = db.cursor()
 
     try:
-        cursor.execute(sql, {'nuevo_telefono':nuevo_telefono, 'nuevo_municipio':nuevo_municipio, 'nombre':nombre,'apellido1':apellido1})
+        cursor.execute(sql, (nuevo_telefono, nuevo_municipio, nombre, apellido1))
         if cursor.rowcount == 0:
-            print("No se ha encontrado conductor con el nombre {nombre} {apellido1}.")
+            print("No se ha encontrado conductor con ese nombre.",nombre, apellido1)
         else:
             db.commit()
-            print("Se ha actualizado correctamente el conductor {nombre} {apellido1}.")
-    except cx_Oracle.Database as e:
-        error, = e.args
-        print("No se ha podido actualizar el conductor: ", error.message)
+            print("Se ha actualizado correctamente el conductor.", nombre, apellido1)
+    except:
         db.rollback()
-    finally:
-        db.close()
+        print("No se ha podido actualizar el conductor.")
+    db.close()

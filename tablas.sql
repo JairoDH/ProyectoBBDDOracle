0d1018e7f56cfe4e2c404ab5175b0db5a36c7f82
JairoDH <123737760+JairoDH@users.noreply.github.com>
2023-03-07 01:05:04 +0100

    Modificaciones
    modificaciones en las restricciones

--- tablas.sql
+++ tablas.sql
@@ -1,19 +1,20 @@
 CREATE TABLE CAMION(
     matricula varchar2(7),
     fecha_alta date,
-    peso_maximo number(7,2) NOT NULL
+    peso_maximo number(7,2) NOT NULL,
+
+CONSTRAINT cp_matricula PRIMARY KEY(matricula),
+CONSTRAINT ch_matri CHECK (REGEXP_LIKE(matricula, '[0-9]{4}[A-Z][A-Z][A-Z]')),
+CONSTRAINT ch_fechalta CHECK (fecha_alta <= to_date('2020-01-01', 'yyyy-mm-dd'))
 );
-ADD CONSTRAINT cp_matricula PRIMARY KEY(matricula)
-ADD CONSTRAINT ch_matri CHECK (REGEXP_LIKE(matricula, '[0-9]{4}[A-Z][A-Z][A-Z]')) 
-ADD CONSTRAINT ch_fechalta CHECK (TO_CHAR(fecha_alta,'YYYY-MM-DD') <= '2020-01-01')
 
 CREATE TABLE CAMION_CONDUCTOR(
     matricula_camion varchar2(7),
     codigo_conductor number(5),
-    fecha date
+    fecha date,
+CONSTRAINT ca_matricami FOREIGN KEY (matricula_camion) references CAMION(matricula),
+CONSTRAINT ca_codicondu FOREIGN KEY (codigo_conductor) references CONDUCTOR(codigo)
 );
-ADD CONSTRAINT ca_matricami FOREIGN KEY (matricula_camion) references CAMION(matricula)
-ADD CONSTRAINT ca_codicondu FOREIGN KEY (codigo_conductor) references CONDUCTOR(codigo)
 
 CREATE TABLE CONDUCTOR(
     codigo number(5),
@@ -25,11 +26,10 @@ CREATE TABLE CONDUCTOR(
     nº_calle number(5),
     provincia varchar2(10),
     poblacion varchar2(10),
-    telefono varchar2(9) NOT NULL
+    telefono varchar2(9) NOT NULL,
+ CONSTRAINT cp_codi PRIMARY KEY (codigo),
+    CONSTRAINT un_dni UNIQUE (DNI),
+    CONSTRAINT ch_dni CHECK (REGEXP_LIKE (DNI, '[0-9]{8}[A-Z]')),
+    CONSTRAINT ch_tele CHECK (REGEXP_LIKE (telefono, '^[679][0-9]{8}$')),
+    CONSTRAINT ch_nom CHECK (nombre = UPPER (nombre)))
 );
-
-ADD CONSTRAINT cp_codi PRIMARY KEY (codigo)
-ADD CONSTRAINT un_dni UNIQUE (DNI)
-ADD CONSTRAINT ch_dni CHECK (REGEXP_LIKE (DNI, '[0-9]{8}[A-Z]'))
-ADD CONSTRAINT ch_tele check (REGEXP_LIKE (telefono, '^[679][0-9]{8}'))
-ADD CONSTRAINT ch_nom check (nombre = UPPER (nombre))
